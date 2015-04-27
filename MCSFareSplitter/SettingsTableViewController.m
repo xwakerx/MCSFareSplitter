807af644ccf0e308ad8e8f9bd1d5304b29e2ddc1
Sergio Jair Cerezo Vallejo <xsergio.jairx@gmail.com>
2015-04-26 20:15:03 -0400

    Settings with logout

---
+++ MCSFareSplitter/SettingsTableViewController.m
@@ -0,0 +1,48 @@
+//
+//  SettingsTableViewController.m
+//  MCSTabSplitter
+//
+//  Created by MCS on 4/26/15.
+//  Copyright (c) 2015 MCS. All rights reserved.
+//
+
+#import "SettingsTableViewController.h"
+#import "TSFacebookController.h"
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+
+@interface SettingsTableViewController ()
+
+@property (weak, nonatomic) IBOutlet UITableViewCell *tvcLogout;
+
+@end
+
+@implementation SettingsTableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+-(void)viewDidAppear:(BOOL)animated{
+    [super viewDidAppear:animated];
+    
+    FBSDKLoginButton *loginButton = [[TSFacebookController sharedController] facebookLoginButton];
+    loginButton.delegate = self;
+    loginButton.center = CGPointMake(self.tvcLogout.frame.size.width/2, self.tvcLogout.frame.size.height/2);
+    [self.tvcLogout addSubview:loginButton];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+-(void)loginButton:(FBSDKLoginButton *)loginButton didCompleteWithResult:(FBSDKLoginManagerLoginResult *)result error:(NSError *)error{
+    
+}
+
+-(void)loginButtonDidLogOut:(FBSDKLoginButton *)loginButton{
+    [self performSegueWithIdentifier:@"logoutSegue" sender:self];
+}
+
+@end
