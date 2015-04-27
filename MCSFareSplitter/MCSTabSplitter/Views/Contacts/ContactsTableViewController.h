2d39a34ed68b619b1e682e0cca9db89762123701
Sergio Jair Cerezo Vallejo <xsergio.jairx@gmail.com>
2015-04-26 22:25:50 -0400

    Contacts displayed in it's corresponding tab

---
+++ MCSFareSplitter/MCSTabSplitter/Views/Contacts/ContactsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ContactsTableViewController.h
+//  MCSTabSplitter
+//
+//  Created by MCS on 4/26/15.
+//  Copyright (c) 2015 MCS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ContactsTableViewController : UITableViewController
+
+@end
